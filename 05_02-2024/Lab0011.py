2de3b8e36001b0dadaec5e0545298fefdbfe962a
akhil-stack <akhilsomashekaran@yahoo.in>
2024-02-09 00:42:17 +0530

    New

---
+++ 05_02-2024/Lab0011.py
@@ -0,0 +1,7 @@
+Number = int(input("enter the number to check:-"))
+print("Entered number is ", Number)
+if (Number % 2) == 0:
+    print("Entered number is even".format(Number))
+else:
+    print("Entered number is odd".format(Number))
+
---
+++ 05_02-2024/Lab0012.py
@@ -0,0 +1,18 @@
+#Format Strings
+
+First_Name = "Amith"
+Last_Name = "Mishra"
+Age = 30
+print(f"Name of the employee is {First_Name} {Last_Name},His Age is {Age}")
+
+num = 90
+print(f"{num} X 1={num}")
+print(f"{num} X 2={num*2}")
+print(f"{num} X 3={num*3}")
+print(f"{num} X 4={num*4}")
+print(f"{num} X 5={num*5}")
+print(f"{num} X 6={num*6}")
+print(f"{num} X 7={num*7}")
+print(f"{num} X 8={num*8}")
+print(f"{num} X 9={num*9}")
+print(f"{num} X 10={num*10}")
