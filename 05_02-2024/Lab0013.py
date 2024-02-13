88a09f69aa147f4feba70e82d920a914b0d00b50
akhil-stack <akhilsomashekaran@yahoo.in>
2024-02-13 16:24:51 +0530

    new code changes

---
+++ 05_02-2024/Lab0013.py
@@ -0,0 +1,11 @@
+Name = "Anbarivu"
+print(len(Name))
+print(Name[7])
+print(Name[len(Name)-1])
+
+# Strings are immutable
+# That means it can't be changed
+String = " Hello"
+String = "Baby"
+# print(String[20]) Index error , String index out of range
+print(String[-1])
